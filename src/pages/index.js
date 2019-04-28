f297f322e6d24cf8bb1801618fe519ff18c28090
Michael Yu <michael.yu2016@gmail.com>
2019-04-27 19:08:20 -0700

    Configure prettier and run on files.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "~/src/components/monads/layout"
-import SEO from "~/src/components/monads/seo"
+import Layout from '~/src/components/monads/layout';
+import SEO from '~/src/components/monads/seo';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`creative`, `labs`, `website`]} />
+    <SEO title='Home' keywords={[`creative`, `labs`, `website`]} />
     <h1>Creative Labs</h1>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to='/page-2/'>Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,16 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "~/src/components/monads/layout"
-import SEO from "~/src/components/monads/seo"
+import Layout from '~/src/components/monads/layout';
+import SEO from '~/src/components/monads/seo';
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Projects" />
+    <SEO title='Projects' />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to='/'>Go back to the homepage</Link>
   </Layout>
-)
+);
 
-export default SecondPage
+export default SecondPage;
