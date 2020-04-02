f413071bce251fe3750cf47aef6ddbbe8e53be76
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2020-04-02 19:57:18 -0400

    Removed unneeded subpages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
 
 import Layout from '../components/monads/layout';
 import SEO from '../components/monads/seo';
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title='404: Not found' />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+export default () => {
+  useEffect(() => {
+    navigate('/');
+  }, [])
 
-export default NotFoundPage;
+  return null;
+};
