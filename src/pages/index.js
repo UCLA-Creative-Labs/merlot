12040ad23217c36108183d73bd65bbe20137cc86
Bryan Pan <bryanpan342@gmail.com>
2020-04-03 20:18:43 -0700

    let's see

--- src/pages/index.js
+++ src/pages/index.js
@@ -99,14 +99,13 @@ function Home() {
               let link = '/projects/' + proj.id;
               const img = (
                 <>
-                <img className='gif'
-                  src={proj.img}
-                  onMouseOver={e => e.currentTarget.style.opacity = '0'}
-                  onMouseOut={e => e.currentTarget.style.opacity = '1'}
-                />
-                <img 
-                  src={proj.alt}
-                />
+                  <img
+                    className='gif'
+                    src={proj.img}
+                    onMouseOver={e => (e.currentTarget.style.opacity = '0')}
+                    onMouseOut={e => (e.currentTarget.style.opacity = '1')}
+                  />
+                  <img src={proj.alt} />
                 </>
               );
 
